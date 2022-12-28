94dbf9b2e020e17b194b12e74f20768d852e1829
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2022-12-28 00:16:26 -0500

    Added db

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# winterbreak22-23
+# Flask Blog
 Building a basic Flask app
 
 Code from: https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import sqlite3
 from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
