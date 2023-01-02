c2eca4022b1c2ec7b0be4f7bfca06a9668c7b27f
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-01-02 07:40:48 -0500

    Added posts

--- app.py
+++ app.py
@@ -10,4 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
